b0f100624f39684ec6851b675b7cc8a010b4d8af
nastya <svistuna4444@gmail.com>
2023-05-28 22:41:46 +0300

    dz5

---
+++ dz5/Program.cs
@@ -0,0 +1,115 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+
+// void FillArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(100, 1000);
+// }
+
+// void PrintArray(int[] array)
+// {
+//     foreach (int item in array)
+//         System.Console.Write($"{item} ");
+//     System.Console.WriteLine();
+// }
+
+// int FindQuantity(int[] array)
+// {
+//     int qua = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] %2 ==0)
+//         qua++;
+//     }
+//     return qua;
+// }
+
+
+
+
+// System.Console.WriteLine();
+// Console.Write("Введите количество элементов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// FillArray(array);
+// System.Console.WriteLine();
+// System.Console.WriteLine("сгенерировался такой массив: ");
+// PrintArray(array);
+// int qua = FindQuantity(array);
+// FindQuantity(array);
+// System.Console.WriteLine();
+// System.Console.Write($"количество четных элементов: {qua}");
+
+
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+// Console.Write($"введите количество элементов массива: ");
+// int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+// int RandomNumbers(int numberElements, int min, int max)
+//   {
+//   int[] randomNumbers = new int[numberElements];
+//   int sumElements = 0;
+//   Console.Write("получившийся массив: ");
+
+//     for (int i = 0; i <randomNumbers.Length; i++ ){
+//       randomNumbers[i] = new Random().Next(min, max);
+
+//       Console.Write(randomNumbers[i] + " ");
+
+//       if (i % 2 != 1)
+//       {
+//         sumElements = sumElements + randomNumbers[i];
+//       }
+//     }
+//   return sumElements;
+//   }
+
+// int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+// Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+
+
+
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+
+
+// double[] arrayRealNumbers = new double[10];
+//   for (int i = 0; i < arrayRealNumbers.Length; i++ )
+//   {
+//     arrayRealNumbers[i] = new Random().Next(1, 10);
+//     Console.Write(arrayRealNumbers[i] + " ");
+//   }
+
+// double maxNumber = arrayRealNumbers[0];
+// double minNumber = arrayRealNumbers[0];
+
+//   for (int i = 1; i < arrayRealNumbers.Length; i++)
+//   {
+//     if (maxNumber < arrayRealNumbers[i])
+//     {
+//       maxNumber = arrayRealNumbers[i];
+//     }
+//         if (minNumber > arrayRealNumbers[i])
+//     {
+//       minNumber = arrayRealNumbers[i];
+//     }
+//   }
+
+//   double decision = maxNumber - minNumber;
+
+//   Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным ({minNumber}) элементами: {decision}");
